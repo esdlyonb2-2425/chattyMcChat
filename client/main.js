19577c09202ab2df51b52eeb4081ed018918acfa
Pierre Lacognata <pierre.lacognata@mail-esd.com>
2024-11-08 11:55:50 +0100

    this time with the code

---
+++ client/main.js
@@ -0,0 +1,16 @@
+const socket = io("ws://localhost:8080");
+const button = document.querySelector("button");
+socket.on('message', (message) => {
+    console.log(message);
+    const ligne = document.createElement('li')
+    ligne.innerHTML = message.author+ " a écrit : "+message.content;
+    document.querySelector('ul').appendChild(ligne)
+})
+
+button.addEventListener('click', (e) => {
+    const toSend = document.querySelector('input').value;
+    socket.emit('message', {
+        content:toSend,
+    });
+})
+
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('serveur socket.io en fonctionnement');
+
+})
+
+const io = socketIo(server, {
+
+    transports: ['websocket', 'polling'],
+    cors: {
+        origin: "*",
+        methods: ["GET","POST"],
+    }
+
+});
+
+
+    io.on('connection', (socket) => {
+        console.log('New user connect to the server');
+        console.log('user-Socker ID :'+ socket.id);
+
+        socket.on('message', (message) => {
+            console.log('Received message from ' + socket.id);
+            console.log(message.content);
+
+            try{
+                io.emit('message',{
+                    author : socket.id,
+                    content: message.content,
+                });
+            }catch (e) {
+                console.log(e);
+            }finally {
+                console.log('Broadcasted message to all clients');
+                console.log(io.sockets.sockets.size);
+            }
+        })
+
+    })
+
+server.listen(8080, ()=>{
+    console.log('Server listening on port 8080');
+});
